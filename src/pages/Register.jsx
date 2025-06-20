47e83652ab0fc65ecb84ef1a198d704c53d7c1bb
vinothelango <vinothknp22@gmail.com>
2025-06-20 18:00:39 +0530

    new adds

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebaseConfig'
 import { useNavigate } from 'react-router-dom'
-
+import './Register.css'
 const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -19,16 +19,16 @@ const Register = () => {
     }
   }
 
-  return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleRegister}>
-        <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  )
+return (
+  <div className="register-container">
+    <h2>Register</h2>
+    <form onSubmit={handleRegister}>
+      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+)
 }
 
 export default Register

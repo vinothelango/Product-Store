c693c27c87982d9ae75ea173a668459305f7be04
vinothelango <vinothknp22@gmail.com>
2025-06-20 15:23:08 +0530

    solves

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',     // <-- Required for Render
+    port: process.env.PORT || 3000, // <-- Render sets PORT env var
+  },
 })
